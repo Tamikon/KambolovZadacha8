6e8e63ed87e062f218ad81cb41e3ec673d269e1e
Сослан Гутнов <rezuandeunezev@gmail.com>
2024-04-09 11:21:22 +0300

    Commit#1

---
+++ KambolovZadacha8/Subjects.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace KambolovZadacha8.Models
+{
+    public class SubjectViewModel
+    {
+        public List<Subject> Subjects { get; set; }
+        public SubjectViewModel()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+}
